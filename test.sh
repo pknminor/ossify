915da8cee9f1d61eaee2305a4ba54cfd01ecd806
Pranav Natesh <pknminor@gmail.com>
2016-04-17 20:08:23 -0700

    updates

--- test.sh
+++ test.sh
@@ -16,7 +16,16 @@ sac "TEST1"
 
 ossify test1 f 1 1 0 ${HOME}/ossify_logs_test
 
-spotify quit
-sac "End test"
+
+# Make sure all processes are killed after
+    # 89:91: execution error: Spotify got an error: Connection is invalid. (-609)
+    # OSSIFY_PAUSE_AT_NEXT_START: seconds played
+    # OSSIFY_PAUSE_AT_NEXT_START: seconds played int
+    # ossify.bash: line 47: 183 -  : syntax error: operand expected (error token is " ")
+
+
+#http://open.spotify.com/track/5A7W9CcRzRt5bjz4HqigFr
+#spotify quit
+#sac "End test"
 
 exit 0
